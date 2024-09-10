86318eb42c024fd4208e716ac4e2fe33a9536da9
RenishSK <renish1248@gmail.com>
2024-09-09 20:26:07 -0400

    First

---
+++ time/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeter from "./Greeter";  // Make sure this path is correct
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
